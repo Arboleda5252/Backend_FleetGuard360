758a3a9a0267fe7cb8306584573be49b2781311e
Arboleda5252 <duvan.arboleda1@udea.edu.co>
2025-06-18 21:02:16 -0500

    jacoco

--- build/generated/sources/annotationProcessor/java/main/com/codefactory/fleetguard360/mapper/TurnoMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/codefactory/fleetguard360/mapper/TurnoMapperImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-18T21:06:47-0500",
-    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.13.jar, environment: Java 17.0.11 (Oracle Corporation)"
+    date = "2025-06-18T20:57:57-0500",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.13.jar, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
 public class TurnoMapperImpl implements TurnoMapper {

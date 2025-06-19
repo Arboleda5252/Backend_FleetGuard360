4c600504807867af6b0f46e47f9c3a3b2f942b39
Arboleda5252 <duvan.arboleda1@udea.edu.co>
2025-06-18 21:46:47 -0500

    jacoco update

--- build/generated/sources/annotationProcessor/java/main/com/codefactory/fleetguard360/mapper/ConductorMapperImpl.java
+++ build/generated/sources/annotationProcessor/java/main/com/codefactory/fleetguard360/mapper/ConductorMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-18T20:57:57-0500",
+    date = "2025-06-18T21:45:31-0500",
     comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.13.jar, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
